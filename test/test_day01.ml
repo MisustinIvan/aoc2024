31bd37b355802e51afaaa90d99baa09395e984cf
Ivan Misustin <ivanmis596@gmail.com>
2024-12-03 00:27:57 +0100

    update printer for day01

--- test/test_day01.ml
+++ test/test_day01.ml
@@ -3,12 +3,14 @@ open OUnit2
 let test_solve_day01_first _ =
     let solution = Days.Day01_first.solve "../inputs/day1_test.txt" in
     let expected_result = 11 in
-    assert_equal expected_result solution
+    assert_equal ~printer:string_of_int
+    expected_result solution
 
 let test_solve_day01_second _ =
     let solution = Days.Day01_second.solve "../inputs/day1_test.txt" in
     let expected_result = 31 in
-    assert_equal expected_result solution
+    assert_equal ~printer:string_of_int
+    expected_result solution
 
 let suite =
     "Day 01 Tests" >:::
