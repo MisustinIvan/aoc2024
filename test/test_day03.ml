568ebfe839a4b10b5a87e9583ab156f9aafef4c1
Ivan Misustin <ivanmis596@gmail.com>
2024-12-03 12:38:16 +0100

    day 3 part 2 test

--- test/test_day03.ml
+++ test/test_day03.ml
@@ -6,11 +6,19 @@ let test_solve_day03_first _ =
     assert_equal ~printer:string_of_int
     expected_result solution
 
+let test_solve_day03_second _ =
+    let solution = Days.Day03_second.solve "../inputs/day3_test.txt" in
+    let expected_result = 48 in
+    assert_equal ~printer:string_of_int
+    expected_result solution
+
+
 
 let suite =
     "Day 03 Tests" >:::
         [
             "test_solve_day03_first" >:: test_solve_day03_first;
+            "test_solve_day03_second" >:: test_solve_day03_second;
         ]
 
 let _ = run_test_tt_main suite
